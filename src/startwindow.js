0f16cbb512603fbe0afb42e484ca5f7226866591
Adam Thompson <arthompson1990@gmail.com>
2026-02-02 15:24:11 +0000

    Add an iterable weak map implementation
    Currently unused but here for use in at least the new observer and live
    collection designs.

--- src/startwindow.js
+++ src/startwindow.js
@@ -89,6 +89,74 @@ this.spdc = function(c, inherit) {
         Object.defineProperty(window[c], "prototype", {value:new inherit})
     Object.defineProperty(window[c].prototype, "dom$class", {value:v})
 }
+/* Extremely useful even if non-standard hence the Eb$ prefix but use a named
+    class as if people see it it really doesn't matter and makes the definition
+    work better.
+
+    Mostly based on the IterableWeakMap example from
+    https://github.com/tc39/proposal-weakrefs with some reformatting.
+
+    It needs to be in here so that the private properties end up as this
+    window's versions of the objects.
+*/
+class Eb$IterableWeakMap {
+    #weakMap = new WeakMap();
+    #refSet = new Set();
+    #finalizationGroup = new FinalizationRegistry(Eb$IterableWeakMap.#cleanup);
+
+    static #cleanup({ set, ref }) {
+        set.delete(ref);
+    }
+
+    constructor(iterable) {
+        for (const [key, value] of iterable) this.set(key, value);
+    }
+
+    set(key, value) {
+        const ref = new WeakRef(key);
+        this.#weakMap.set(key, { value, ref });
+        this.#refSet.add(ref);
+        this.#finalizationGroup.register(key, {
+            set: this.#refSet,
+            ref
+        }, ref);
+    }
+
+    get(key) {
+        const entry = this.#weakMap.get(key);
+        return entry && entry.value;
+    }
+
+    delete(key) {
+        const entry = this.#weakMap.get(key);
+        if (!entry) return false;
+        this.#weakMap.delete(key);
+        this.#refSet.delete(entry.ref);
+        this.#finalizationGroup.unregister(entry.ref);
+        return true;
+    }
+
+    *[Symbol.iterator]() {
+        for (const ref of this.#refSet) {
+            const key = ref.deref();
+            if (!key) continue;
+            const { value } = this.#weakMap.get(key);
+            yield [key, value];
+        }
+    }
+
+    entries() {
+        return this[Symbol.iterator]();
+    }
+
+    *keys() {
+        for (const [key, value] of this)  yield key;
+    }
+
+    *values() {
+        for (const [key, value] of this)  yield value;
+    }
+}
 
 // The first DOM class is Node, at the head of all else.
 swm("Node", function(){})
