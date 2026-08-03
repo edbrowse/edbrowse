27b4e5d536c883c0bb0cbf5a3ae7770a590b775b
Karl Dahlke <eklhad@comcast.net>
2026-08-03 15:00:10 -0400

    Export the C makeBoundary() function to js. Yeah, it would have been easier to write a js version, but this way everything is consistent.

--- src/jseng-quick.c
+++ src/jseng-quick.c
@@ -1702,6 +1702,15 @@ empty:
 }
 #endif
 
+static JSValue nat_makeBoundary(JSContext * cx, JSValueConst this, int argc, JSValueConst *argv)
+{
+    JSValue v = JS_NewString(cx, makeBoundary());
+    (void) this;
+    (void) argc;
+    (void) argv;
+    return v;
+}
+
 // object keys, just for debugging from jdb
 static JSValue nat_ok(JSContext * cx, JSValueConst this, int argc, JSValueConst *argv)
 {
@@ -3770,6 +3779,8 @@ JS_NewCFunction(mwc, nat_btoa, "btoa", 1), JS_PROP_ENUMERABLE);
     JS_DefinePropertyValueStr(mwc, mwo, "atob",
 JS_NewCFunction(mwc, nat_atob, "atob", 1), JS_PROP_ENUMERABLE);
 #endif
+    JS_DefinePropertyValueStr(mwc, mwo, "makeBoundary",
+JS_NewCFunction(mwc, nat_makeBoundary, "makeBoundary", 0), 0);
     JS_DefinePropertyValueStr(mwc, mwo, "eb$voidfunction",
 JS_NewCFunction(mwc, nat_void, "void", 0), JS_PROP_ENUMERABLE);
     JS_DefinePropertyValueStr(mwc, mwo, "eb$nullfunction",
