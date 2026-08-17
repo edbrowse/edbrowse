b4d84fac876413169e4662d8a2868381f5711018
Karl Dahlke <eklhad@comcast.net>
2026-08-17 06:30:13 -0400

    Add a test that exercises tracing. This is partly for documentation purposes. Tracing, as part of debugging, is not a simple process. So the comments in run-trace-alpine might be the most important thing.

--- src/main.c
+++ src/main.c
@@ -12,7 +12,7 @@ Frame *cf;
 int gfsn; // global frame sequence number
 const char *progname;
 const char eol[] = "\r\n";
-const char *version = "3.8.18";
+const char *version = "3.8.18+";
 char *changeFileName;
 char *configFile, *addressFile, *cookieFile, *emojiFile, *configHome;
 char *mailDir, *mailUnread, *mailStash, *mailReply;
